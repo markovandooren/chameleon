02f26ede9d5f7e644c7f9611a30ea09eb4b21377
Marko van Dooren <marko@10-92-126-134.kotnet.org>
2008-10-27 20:19:49 +0100

    initial import

---
+++ src/chameleon/core/context/DeclarationCollector.java
@@ -0,0 +1,64 @@
+package chameleon.core.context;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import chameleon.core.MetamodelException;
+import chameleon.core.declaration.Declaration;
+import chameleon.core.declaration.DeclarationContainer;
+import chameleon.core.declaration.DeclarationSelector;
+
+public class DeclarationCollector<E extends DeclarationContainer> {
+
+	public DeclarationCollector(E element) {
+		_element = element;
+	}
+	
+	private E _element;
+
+	/**
+	 * Return the element referenced by this collector
+	 * @return
+	 */
+	public E element() {
+	  return _element; 
+	}
+
+	/**
+	 * Return the declarations declared on-demand by the referenced element.
+	 * 
+	 * The default implementation returns an empty set.
+	 * @throws MetamodelException 
+	 */
+  public Set<Declaration> demandDeclarations() throws MetamodelException {
+  	return new HashSet<Declaration>();
+  }
+  
+	/**
+	 * Return the declarations directly declared by the referenced element.
+	 * @throws MetamodelException 
+	 */
+  public Set<Declaration> directDeclarations() throws MetamodelException {
+  	return element().declarations(); 
+  }
+  
+  /**
+   * Return those declarations of this declaration container that are selected
+   * by the given signature selector. First the direct declarations are searched.
+   * If that yields no results, the on-demand declarations are searched.
+   * 
+   * @param <T> The type of the arguments selected by the given signature selector. This type
+   *            shoud be inferred automatically.
+   * @param selector
+   * @return
+   * @throws MetamodelException
+   */
+  public <T extends Declaration> Set<T> declarations(DeclarationSelector<T> selector) throws MetamodelException {
+    Set<T> result = selector.selection(directDeclarations());
+    // Only use on-demand declarations when no direct declarations are found.
+    if(result.isEmpty()) {
+    	result = selector.selection(demandDeclarations());
+    }
+    return result;
+  }
+}
