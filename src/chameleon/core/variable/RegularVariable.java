96859310b358680168ab4b38aaf6cc3fc3d169e2
marko <marko@192.168.0.3>
2008-11-10 11:41:10 +0100

    restructuring variable hierarchy

--- src/chameleon/core/variable/RegularVariable.java
+++ src/chameleon/core/variable/RegularVariable.java
@@ -1,14 +1,35 @@
 package chameleon.core.variable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rejuse.association.OrderedReferenceSet;
 import org.rejuse.association.Reference;
+import org.rejuse.java.collections.Visitor;
+import org.rejuse.logic.ternary.Ternary;
+import org.rejuse.property.Property;
+import org.rejuse.property.PropertySet;
 
+import chameleon.core.MetamodelException;
+import chameleon.core.context.TargetContext;
 import chameleon.core.declaration.SimpleNameSignature;
+import chameleon.core.element.Element;
+import chameleon.core.expression.Expression;
+import chameleon.core.expression.ExpressionContainer;
+import chameleon.core.modifier.Modifier;
+import chameleon.core.statement.CheckedExceptionList;
+import chameleon.core.statement.ExceptionSource;
+import chameleon.core.type.Type;
+import chameleon.core.type.TypeDescendantImpl;
 import chameleon.core.type.TypeReference;
+import chameleon.util.Util;
 
-public abstract class RegularVariable<E extends RegularVariable<E,P>, P extends VariableContainer> extends Variable<E,P> {
+public abstract class RegularVariable<E extends RegularVariable<E,P>, P extends VariableContainer> 
+       extends TypeDescendantImpl<E, P>
+       implements Variable<E,P>, ExpressionContainer<E,P>, ExceptionSource<E,P> {
 
 	public RegularVariable(SimpleNameSignature sig, TypeReference typeRef) {
-		super(sig);
+		setSignature(sig);
     setTypeReference(typeRef);
 	}
 
@@ -26,5 +47,162 @@ public abstract class RegularVariable<E extends RegularVariable<E,P>, P extends 
     _typeReference.connectTo(type.getParentLink());
   }
 
+	/**
+	 * INITIALIZATION EXPRESSION 
+	 */
+  
+	private Reference<RegularVariable,Expression> _init = new Reference<RegularVariable,Expression>(this);
+
+	public Expression getInitialization() {
+    return _init.getOtherEnd();
+  }
+  
+  public void setInitialization(Expression expr) {
+    if(expr != null) {
+      _init.connectTo(expr.getParentLink());
+    }
+    else {
+      _init.connectTo(null);
+    }
+  }
+  
+  public CheckedExceptionList getCEL() throws MetamodelException {
+    if(getInitialization() != null) {
+      return getInitialization().getCEL();
+    }
+    else {
+      return new CheckedExceptionList(getNamespace().language());
+    }
+  }
+
+  public CheckedExceptionList getAbsCEL() throws MetamodelException {
+    if(getInitialization() != null) {
+      return getInitialization().getAbsCEL();
+    }
+    else {
+      return new CheckedExceptionList(getNamespace().language());
+    }
+  }
+
+ /*@
+   @ also public behavior
+   @
+   @ post \result.containsAll(modifiers());
+   @ post getTypeReference() != null ==> \result.contains(getTypeReference());
+   @ post getInitialization() != null ==> \result.contains(getInitialization());
+   @ post signature() != null ==> \result.contains(signature());
+   @*/
+  public List<Element> getChildren() {
+    List result = new ArrayList<Element>();
+    Util.addNonNull(getInitialization(), result);
+    result.addAll(modifiers());
+    Util.addNonNull(signature(), result);
+    Util.addNonNull(getInitialization(), result);
+    return result;
+  }
+	/*************
+	 * MODIFIERS *
+	 *************/
+	
+	private OrderedReferenceSet<Variable, Modifier> _modifiers = new OrderedReferenceSet<Variable, Modifier>(this);
+
+	public List<Modifier> modifiers() {
+		return _modifiers.getOtherEnds();
+	}
+
+	public void addModifier(Modifier modifier) {
+		if ((!_modifiers.contains(modifier.getParentLink())) && (modifier != null)) {
+			_modifiers.add(modifier.getParentLink());
+		}
+	}
+
+	public void removeModifier(Modifier modifier) {
+		_modifiers.remove(modifier.getParentLink());
+	}
+
+	public boolean hasModifier(Modifier modifier) {
+		return _modifiers.getOtherEnds().contains(modifier);
+	}
+
+  public void setSignature(SimpleNameSignature signature) {
+    if(signature != null) {
+      _signature.connectTo(signature.getParentLink());
+    } else {
+      _signature.connectTo(null);
+    }
+  }
+  
+  /**
+   * Return the signature of this member.
+   */
+  public SimpleNameSignature signature() {
+    return _signature.getOtherEnd();
+  }
+  
+  private Reference<RegularVariable, SimpleNameSignature> _signature = new Reference<RegularVariable, SimpleNameSignature>(this);
+
+  /**
+   * Return the name of this variable.
+   */
+  public String getName() {
+    return signature().getName();
+  }
+
+  public Type getType() throws MetamodelException {
+    Type result = getTypeReference().getType();
+    if(result != null) {
+      return result;
+    }
+    else {
+      getTypeReference().getType();
+      throw new MetamodelException();
+    }
+  }
+
+  /*@
+  @ also public behavior
+  @
+  @ post \result == getParent().getNearestType();
+  @*/
+ public Type getNearestType() {
+   return getParent().getNearestType();
+ }
+
+ public E clone() {
+   final E result = cloneThis();
+   new Visitor<Modifier>() {
+     public void visit(Modifier element) {
+       result.addModifier(element.clone());
+     }
+   }.applyTo(modifiers());
+   return result;
+ }
+
+ protected abstract E cloneThis();
+
+ public Ternary is(Property<Element> property) {
+   PropertySet<Element> declared = declaredProperties();
+   if((property).appliesTo(this)) {
+     declared.add(property);
+   }
+   return declared.implies(property);
+ }
+
+ public PropertySet<Element> declaredProperties() {
+   PropertySet<Element> result = new PropertySet<Element>();
+   for(Modifier modifier:modifiers()) {
+     result.addAll(modifier.impliedProperties());
+   }
+   return result;
+ }
+ 
+ public TargetContext targetContext() throws MetamodelException {
+   return getType().targetContext();
+ }
+
+
+ public Variable resolve() {
+ 	return this;
+ }
 
 }
--- src/chameleon/core/variable/Variable.java
+++ src/chameleon/core/variable/Variable.java
@@ -2,7 +2,6 @@ package chameleon.core.variable;
 
 import java.util.List;
 
-import org.rejuse.association.OrderedReferenceSet;
 import org.rejuse.association.Reference;
 import org.rejuse.java.collections.Visitor;
 import org.rejuse.logic.ternary.Ternary;
@@ -17,7 +16,7 @@ import chameleon.core.element.Element;
 import chameleon.core.modifier.Modifier;
 import chameleon.core.modifier.ModifierContainer;
 import chameleon.core.type.Type;
-import chameleon.core.type.TypeDescendantImpl;
+import chameleon.core.type.TypeDescendant;
 import chameleon.core.type.TypeReference;
 import chameleon.core.type.VariableOrType;
 import chameleon.util.Util;
@@ -25,50 +24,25 @@ import chameleon.util.Util;
 /**
  * @author Marko van Dooren
  */
-public abstract class Variable<E extends Variable<E,P>, P extends VariableContainer> 
-                extends TypeDescendantImpl<E,P> 
-                implements VariableOrType<E,P>,ModifierContainer<E,P>, TargetDeclaration<E,P,SimpleNameSignature>{
+public interface Variable<E extends Variable<E,P>, P extends VariableContainer> 
+                extends TypeDescendant<E,P>, 
+                VariableOrType<E,P>,ModifierContainer<E,P>, TargetDeclaration<E,P,SimpleNameSignature> {
 
-  public Variable(SimpleNameSignature sig) {
-    setSignature(sig);
-  }
-  
-  public void setSignature(SimpleNameSignature signature) {
-    if(signature != null) {
-      _signature.connectTo(signature.getParentLink());
-    } else {
-      _signature.connectTo(null);
-    }
-  }
+  public void setSignature(SimpleNameSignature signature);
   
   /**
    * Return the signature of this member.
    */
-  public SimpleNameSignature signature() {
-    return _signature.getOtherEnd();
-  }
+  public SimpleNameSignature signature();
   
-  private Reference<Variable, SimpleNameSignature> _signature = new Reference<Variable, SimpleNameSignature>(this);
-
   /**
    * Return the name of this variable.
    */
-  public String getName() {
-    return signature().getName();
-  }
+  public String getName();
 
   public abstract TypeReference getTypeReference();
 
-  public Type getType() throws MetamodelException {
-    Type result = getTypeReference().getType();
-    if(result != null) {
-      return result;
-    }
-    else {
-      getTypeReference().getType();
-      throw new MetamodelException();
-    }
-  }
+  public Type getType() throws MetamodelException;
 
 
 
@@ -76,81 +50,27 @@ public abstract class Variable<E extends Variable<E,P>, P extends VariableContai
 	 * MODIFIERS *
 	 *************/
 	
-	private OrderedReferenceSet<Variable, Modifier> _modifiers = new OrderedReferenceSet<Variable, Modifier>(this);
+	public List<Modifier> modifiers();
 
-	public List<Modifier> modifiers() {
-		return _modifiers.getOtherEnds();
-	}
+	public void addModifier(Modifier modifier);
 
-	public void addModifier(Modifier modifier) {
-		if ((!_modifiers.contains(modifier.getParentLink())) && (modifier != null)) {
-			_modifiers.add(modifier.getParentLink());
-		}
-	}
-
-	public void removeModifier(Modifier modifier) {
-		_modifiers.remove(modifier.getParentLink());
-	}
-
-	public boolean is(Modifier modifier) {
-		return _modifiers.getOtherEnds().contains(modifier);
-	}
+	public void removeModifier(Modifier modifier);
 
  /*@
    @ also public behavior
    @
    @ post \result == getParent().getNearestType();
    @*/
-  public Type getNearestType() {
-    return getParent().getNearestType();
-  }
+  public Type getNearestType();
 
-  public E clone() {
-    final E result = cloneThis();
-    new Visitor<Modifier>() {
-      public void visit(Modifier element) {
-        result.addModifier(element.clone());
-      }
-    }.applyTo(modifiers());
-    return result;
-  }
+  public E clone();
 
-  protected abstract E cloneThis();
-
- /*@
-   @ also public behavior
-   @
-   @ post \result.containsAll(getModifiers());
-   @ post getTypeReference() != null ==> \result.contains(getTypeReference());
-   @*/
-  public List getChildren() {
-    List result = modifiers();
-    Util.addNonNull(getTypeReference(), result);
-    return result;
-  }
-  
-  public Ternary is(Property<Element> property) {
-    PropertySet<Element> declared = declaredProperties();
-    if((property).appliesTo(this)) {
-      declared.add(property);
-    }
-    return declared.implies(property);
-  }
+  public Ternary is(Property<Element> property);
  
-  public PropertySet<Element> declaredProperties() {
-    PropertySet<Element> result = new PropertySet<Element>();
-    for(Modifier modifier:modifiers()) {
-      result.addAll(modifier.impliedProperties());
-    }
-    return result;
-  }
+  public PropertySet<Element> declaredProperties();
   
-  public TargetContext targetContext() throws MetamodelException {
-    return getType().targetContext();
-  }
+  public TargetContext targetContext() throws MetamodelException;
 
 
-  public Variable resolve() {
-  	return this;
-  }
+  public Variable resolve();
 }
