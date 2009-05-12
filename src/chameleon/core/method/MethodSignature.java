eed7979f0d556c6790b230452f7d4eb3bfd423a3
marko <marko@objectron.dyn.cs.kuleuven.be>
2009-05-12 18:17:31 +0200

    init

---
+++ src/chameleon/core/method/MethodSignature.java
@@ -0,0 +1,7 @@
+package chameleon.core.method;
+
+import chameleon.core.declaration.Signature;
+
+public abstract class MethodSignature<E extends MethodSignature,P extends Method> extends Signature<E, P> {
+
+}
