e457063f535efde25d98eb0e81095754fdedbfdb
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-08-21 20:17:06 +0200

    moved stuff back

--- src/chameleon/input/MetaModelFactory.java
+++ src/chameleon/input/MetaModelFactory.java
@@ -4,10 +4,11 @@ import java.io.File;
 import java.util.Set;
 
 import chameleon.core.namespace.Namespace;
+import chameleon.linkage.ILinkageFactory;
 
 public interface MetaModelFactory {
 
-//	public Namespace getMetaModel(ILinkageFactory fact, Set<File> files) throws Exception;
+	public Namespace getMetaModel(ILinkageFactory fact, Set<File> files) throws Exception;
 	
 	public Set loadFiles(String path, String extension, boolean recursive);
 }
