cde77c08f00cd7e6a8b3b61d449b2e9eefeabedc
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-08-21 20:11:45 +0200

    removed methods that should not be there

--- src/chameleon/input/MetaModelFactory.java
+++ src/chameleon/input/MetaModelFactory.java
@@ -4,11 +4,10 @@ import java.io.File;
 import java.util.Set;
 
 import chameleon.core.namespace.Namespace;
-import chameleon.editor.toolextension.ILinkageFactory;
 
 public interface MetaModelFactory {
 
-	public Namespace getMetaModel(ILinkageFactory fact, Set<File> files) throws Exception;
+//	public Namespace getMetaModel(ILinkageFactory fact, Set<File> files) throws Exception;
 	
 	public Set loadFiles(String path, String extension, boolean recursive);
 }
