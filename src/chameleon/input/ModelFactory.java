ca6671623741d6cc3a22aef9c2db88d7a0986ffb
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-08-27 18:49:51 +0200

    working on editor

--- src/chameleon/input/ModelFactory.java
+++ src/chameleon/input/ModelFactory.java
@@ -3,12 +3,15 @@ package chameleon.input;
 import java.io.File;
 import java.util.Set;
 
+import chameleon.core.element.Element;
 import chameleon.core.namespace.Namespace;
 import chameleon.tool.Connector;
 
 public interface ModelFactory extends Connector {
 
-	public Namespace getMetaModel(Set<File> files) throws Exception;
+	public Namespace createModel(Set<File> files) throws Exception;
 	
-	public Set loadFiles(String path, String extension, boolean recursive);
+	public void reParse(Element element);
+	
+//	public Set loadFiles(String path, String extension, boolean recursive);
 }
--- src/chameleon/input/ParseException.java
+++ src/chameleon/input/ParseException.java
@@ -1,18 +1,27 @@
 package chameleon.input;
 
+import chameleon.core.compilationunit.CompilationUnit;
+
 public class ParseException extends Exception {
 
-	  public ParseException() {
-	  	
-	  }
-	  public ParseException(String msg) {
-	  	super(msg);
-	  }
-	  public ParseException(Exception exc) {
-	  	super(exc);
-	  }
-	  public ParseException(String msg, Exception exc) {
-	  	super(msg, exc);
+	  public ParseException(CompilationUnit compilationUnit) {
+	  	this(null,null,compilationUnit);
 	  }
 	  
+	  public ParseException(String msg,CompilationUnit compilationUnit) {
+	  	this(msg,null,compilationUnit);
+	  }
+	  public ParseException(Exception exc,CompilationUnit compilationUnit) {
+	  	this(null,exc,compilationUnit);
+	  }
+	  public ParseException(String msg, Exception exc,CompilationUnit compilationUnit) {
+	  	super(msg, exc);
+	  	_compilationUnit = compilationUnit;
+	  }
+	  
+	  private CompilationUnit _compilationUnit;
+	  
+	  public CompilationUnit compilationUnit() {
+	  	return _compilationUnit;
+	  }
 }
