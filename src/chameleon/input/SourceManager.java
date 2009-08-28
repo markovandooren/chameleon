53d8c843855ef35f179981d1bb1320903b4542b1
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-08-28 14:20:14 +0200

    added source manager

---
+++ src/chameleon/input/SourceManager.java
@@ -0,0 +1,10 @@
+package chameleon.input;
+
+import chameleon.core.element.Element;
+import chameleon.tool.Connector;
+
+public interface SourceManager extends Connector {
+	
+	public String text(Element element);
+
+}
