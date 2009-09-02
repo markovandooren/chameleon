21ae91c83fe5e60fff0c3a63cee980fb5c882dd1
marko <marko@10-92-113-181.kotnet.org>
2009-09-02 23:59:58 +0200

    disable caching by default. that does not work if the editor reparses elements!

--- src/chameleon/core/Config.java
+++ src/chameleon/core/Config.java
@@ -6,24 +6,31 @@ public class Config {
 	 * OPTIMISATION CONSTANTS *
 	 **************************/
 	
-	public final static boolean DEBUG=true;
+	public static boolean DEBUG=true;
 	
-	public final static boolean CACHE_ALL_TYPE_NAMES = true;
+	public static boolean CACHE_ALL_TYPE_NAMES = false;
 	
-	public static boolean CACHE_DECLARATIONS = true;
+	public static boolean CACHE_DECLARATIONS = false;
 
 	/**
 	 * Turn cache of element references on and off. Default is on.
 	 */
-	public static boolean CACHE_ELEMENT_REFERENCES = true;
+	public static boolean CACHE_ELEMENT_REFERENCES = false;
 	
 	/**
 	 * Turn cache of expression types on and off. Default is on.
 	 */
-	public static boolean CACHE_EXPRESSION_TYPES = true;
+	public static boolean CACHE_EXPRESSION_TYPES = false;
 
 	/**
 	 * Turn cache of language on and off. Default is on.
 	 */
-	public static boolean CACHE_LANGUAGE = true;
+	public static boolean CACHE_LANGUAGE = false;
+
+	public static void setCaching(boolean bool) {
+		CACHE_ALL_TYPE_NAMES = bool;
+		CACHE_DECLARATIONS = bool;
+		CACHE_ELEMENT_REFERENCES = bool;
+		CACHE_LANGUAGE = bool;
+	}
 }
