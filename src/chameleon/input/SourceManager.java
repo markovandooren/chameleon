1b8a1397d5e4d6b55155e68f883c597b8d8a37ab
marko <marko@objectron.dyn.cs.kuleuven.be>
2009-11-20 15:35:37 +0100

    added NoLocationException

--- src/chameleon/input/SourceManager.java
+++ src/chameleon/input/SourceManager.java
@@ -3,8 +3,16 @@ package chameleon.input;
 import chameleon.core.element.Element;
 import chameleon.tool.Connector;
 
+/**
+ * A connector that allows a tool to retrieve the 'source code' of a particular element.
+ * 
+ * @author Marko van Dooren
+ */
 public interface SourceManager extends Connector {
 	
-	public String text(Element element);
+	/**
+	 * Return the text representing the 'source code' for the given element.
+	 */
+	public String text(Element element) throws NoLocationException;
 
 }
