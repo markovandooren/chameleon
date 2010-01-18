447fab5d49ad2a08516bdf002558834d0bdf59f2
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2010-01-18 17:09:43 +0100

    finished implementation

---
+++ src/chameleon/core/declaration/DeclarationAlias.java
@@ -0,0 +1,88 @@
+package chameleon.core.declaration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rejuse.association.SingleAssociation;
+
+import chameleon.core.MetamodelException;
+import chameleon.core.element.Element;
+import chameleon.core.lookup.LookupException;
+import chameleon.core.namespace.NamespaceElementImpl;
+import chameleon.core.scope.Scope;
+import chameleon.core.validation.BasicProblem;
+import chameleon.core.validation.Valid;
+import chameleon.core.validation.VerificationResult;
+
+public class DeclarationAlias<S extends Signature,D extends Declaration> extends NamespaceElementImpl<DeclarationAlias<S,D>, Element> implements Declaration<DeclarationAlias<S,D>, Element, S, D> {
+
+	public DeclarationAlias(S signature, D declaration) {
+		setSignature(signature);
+ 		_aliasedDeclaration = declaration;
+	}
+	
+	@Override
+	public DeclarationAlias clone() {
+		return new DeclarationAlias<Signature, Declaration>(signature().clone(), aliasedDeclaration());
+	}
+
+	@Override
+	public VerificationResult verifySelf() {
+		VerificationResult result = Valid.create();
+		if(aliasedDeclaration() == null) {
+			result = result.and(new BasicProblem(this, "The alias with signature "+signature().toString()+" does not alias any declaration."));
+		}
+		if(signature() == null) {
+			result = result.and(new BasicProblem(this, "The signature of the alias is null, but it should be non-null."));
+		}
+		return result;
+	}
+
+	public D aliasedDeclaration() {
+		return _aliasedDeclaration;
+	}
+
+	public D actualDeclaration() {
+		return _aliasedDeclaration;
+	}
+
+	/**
+	 * Return the scope of the aliased declaration. Since an alias is just another name for a declaration,
+	 * the scope does not change.
+	 */
+ /*@
+   @ public behavior
+   @
+   @ post \result == actualDeclaration.scope();
+   @
+   @ signals(MetamodelException) actualDeclaration() == null;
+   @*/
+	public Scope scope() throws MetamodelException {
+		D aliasedDeclaration = aliasedDeclaration();
+		if(aliasedDeclaration != null) {
+		  return aliasedDeclaration.scope();
+		} else {
+			throw new MetamodelException("The scope of the alias cannot be determined because the aliased declaration is null.");
+		}
+	}
+
+	public Declaration<?, ?, ?, D> selectionDeclaration() throws LookupException {
+		return _aliasedDeclaration;
+	}
+
+	public List<? extends Element> children() {
+		return new ArrayList<Element>();
+	}
+	
+	public void setSignature(S signature) {
+		setAsParent(_signature, signature);
+	}
+	
+	public S signature() {
+		return _signature.getOtherEnd();
+	}
+
+	private SingleAssociation<DeclarationAlias, S> _signature = new SingleAssociation<DeclarationAlias, S>(this); 
+
+	private D _aliasedDeclaration;
+}
