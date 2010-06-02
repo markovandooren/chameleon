3dbb7f0552cf2239a3eeb35e0ebc72c43ef93ac4
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2010-06-02 15:32:11 +0200

    init

---
+++ src/chameleon/oo/type/NonLocalTypeReference.java
@@ -0,0 +1,88 @@
+package chameleon.oo.type;
+
+import java.util.List;
+
+import org.rejuse.association.SingleAssociation;
+
+import chameleon.core.declaration.Declaration;
+import chameleon.core.element.Element;
+import chameleon.core.lookup.LookupException;
+import chameleon.core.lookup.LookupStrategy;
+import chameleon.core.namespace.NamespaceElementImpl;
+import chameleon.core.validation.Valid;
+import chameleon.core.validation.VerificationResult;
+import chameleon.oo.language.ObjectOrientedLanguage;
+import chameleon.util.Util;
+
+public abstract class NonLocalTypeReference<E extends NonLocalTypeReference> extends NamespaceElementImpl<E,Element> implements TypeReference<E> {
+
+	public NonLocalTypeReference(TypeReference tref) {
+	   this(tref,tref.parent());
+		}
+		
+		public NonLocalTypeReference(TypeReference tref, Element lookupParent) {
+		   setActualReference(tref);
+			_lookupParent = lookupParent;
+		}
+	
+	public TypeReference actualReference() {
+		return _actual.getOtherEnd();
+	}
+
+	public void setActualReference(TypeReference actual) {
+		setAsParent(_actual, actual);
+	}
+
+	private SingleAssociation<NonLocalTypeReference, TypeReference> _actual = new SingleAssociation<NonLocalTypeReference, TypeReference>(this);
+
+	@Override
+	public LookupStrategy lexicalLookupStrategy() throws LookupException {
+		return lookupParent().lexicalLookupStrategy(this);
+	}
+
+	public void setLookupParent(Element newParent) {
+		_lookupParent = newParent;
+	}
+	
+	public Element lookupParent() {
+		return _lookupParent;
+	}
+
+	private Element _lookupParent;
+
+	@Override
+	public VerificationResult verifySelf() {
+		return Valid.create();
+	}
+
+	public Type getElement() throws LookupException {
+		return actualReference().getElement();
+	}
+
+	public Type getType() throws LookupException {
+		return getElement();
+	}
+
+	public TypeReference intersection(TypeReference other) {
+		return other.intersectionDoubleDispatch(this);
+	}
+
+	public TypeReference intersectionDoubleDispatch(TypeReference other) {
+		return language(ObjectOrientedLanguage.class).createIntersectionReference(clone(), other.clone());
+	}
+
+	public TypeReference intersectionDoubleDispatch(IntersectionTypeReference<?> other) {
+		IntersectionTypeReference<?> result = other.clone();
+		result.add(clone());
+		return result;
+	}
+
+	public Declaration getDeclarator() throws LookupException {
+		return actualReference().getDeclarator();
+	}
+
+	public List<? extends Element> children() {
+		return Util.createNonNullList(actualReference());
+	}
+
+}
