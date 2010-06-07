417aa32d2f0fcafc56b59029568c6350abfe3a59
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2010-06-07 23:30:05 +0200

    moved the variable declaration/declarator classes, introduce typeparametersubstitution

---
+++ src/chameleon/core/variable/VariableDeclarator.java
@@ -0,0 +1,23 @@
+package chameleon.core.variable;
+
+import java.util.List;
+
+import chameleon.core.declaration.DeclarationContainer;
+import chameleon.core.declaration.SimpleNameSignature;
+import chameleon.core.element.Element;
+import chameleon.core.expression.Expression;
+import chameleon.core.modifier.Modifier;
+import chameleon.oo.type.TypeReference;
+
+public interface VariableDeclarator<E extends VariableDeclarator, V extends Variable, P extends Element> extends Element<E,P> , DeclarationContainer<E, P> {
+
+	public List<VariableDeclaration<V>> variableDeclarations();
+
+	public V createVariable(SimpleNameSignature signature, Expression expression);
+	
+	public TypeReference typeReference();
+	
+	public List<Modifier> modifiers();
+
+//	public Type type() throws LookupException;
+}
