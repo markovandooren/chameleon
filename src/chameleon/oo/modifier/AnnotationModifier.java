d22bce455d254354a9dad8260635ec18188f8279
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2011-10-09 20:25:10 +0200

    backup

---
+++ src/chameleon/oo/modifier/AnnotationModifier.java
@@ -0,0 +1,33 @@
+package chameleon.oo.modifier;
+
+import org.rejuse.property.PropertySet;
+
+import chameleon.core.element.Element;
+import chameleon.core.modifier.ModifierImpl;
+import chameleon.core.property.ChameleonProperty;
+
+public class AnnotationModifier extends ModifierImpl<AnnotationModifier> {
+
+	public AnnotationModifier(String name) {
+		setName(name);
+	}
+	
+	@Override
+	public AnnotationModifier clone() {
+		return new AnnotationModifier(name());
+	}
+
+	private String _name;
+	
+	public String name() {
+		return _name;
+	}
+	
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public PropertySet<Element, ChameleonProperty> impliedProperties() {
+		return new PropertySet<Element, ChameleonProperty>();
+	}
+}
