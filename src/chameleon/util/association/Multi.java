aeef33b9b6e662b893ad00cc8c60778cb3677489
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-05-06 19:16:52 +0200

    backup

--- src/chameleon/util/association/Multi.java
+++ src/chameleon/util/association/Multi.java
@@ -3,17 +3,34 @@ package chameleon.util.association;
 import org.rejuse.association.OrderedMultiAssociation;
 
 import chameleon.core.element.Element;
+import chameleon.core.validation.BasicProblem;
+import chameleon.core.validation.Valid;
+import chameleon.core.validation.VerificationResult;
 
 public class Multi<T extends Element> extends OrderedMultiAssociation<Element, T> {
 
 	public Multi(Element element) {
 		super(element);
+		_max = -1;
 	}
 	
 	public Multi(Element element, int min, int max) {
 		this(element);
+		if(min > max) {
+			throw new IllegalArgumentException();
+		}
 		_min=min;
 		_max=max;
+	}
+	
+	public Multi(Element element, String role) {
+		this(element);
+		setRole(role);
+	}
+	
+	public Multi(Element element, int min, int max, String role) {
+		this(element,min,max);
+		setRole(role);
 	}
 	
 	public int min() {
@@ -26,5 +43,33 @@ public class Multi<T extends Element> extends OrderedMultiAssociation<Element, T
 	
 	private int _min;
 	private int _max;
+	
+	public boolean isConstrained() {
+		return _max < 1;
+	}
+	
+	public VerificationResult verify() {
+		VerificationResult result = Valid.create();
+		if(isConstrained()) {
+			int size = size();
+			if(size < min()) {
+				result = result.and(new BasicProblem(getObject(), "At least " + min()+" " + role() + " were expected, but only "+size+ " are defined."));
+			}
+			if(size > max()) {
+				result = result.and(new BasicProblem(getObject(), "At most " + max()+" " + role() +" were expected, but "+size+ " are defined."));
+			}
+		}
+		return result;
+	}
+	
+	public String role() {
+		return _role;
+	}
+	
+	public void setRole(String role) {
+		_role = role;
+	}
+	
+	private String _role = "elements";
 
 }
--- src/chameleon/util/association/Single.java
+++ src/chameleon/util/association/Single.java
@@ -4,6 +4,9 @@ import org.rejuse.association.Association;
 import org.rejuse.association.SingleAssociation;
 
 import chameleon.core.element.Element;
+import chameleon.core.validation.BasicProblem;
+import chameleon.core.validation.Valid;
+import chameleon.core.validation.VerificationResult;
 
 public class Single<T extends Element> extends SingleAssociation<Element, T> {
 
@@ -30,4 +33,54 @@ public class Single<T extends Element> extends SingleAssociation<Element, T> {
 	public boolean mandatory() {
 		return _mandatory;
 	}
+	
+	public VerificationResult verify() {
+		VerificationResult result = Valid.create();
+		if(mandatory()) {
+			if(mandatory() && getOtherEnd() == null) {
+				result = result.and(new BasicProblem(getObject(), "One " + role() + " was expected, but none is defined."));
+			}
+		}
+		return result;
+	}
+
+	public String role() {
+		return _role;
+	}
+	
+	public void setRole(String role) {
+		_role = role;
+	}
+	
+	private String _role = "element";
+	
+	@Override
+	public T getOtherEnd() {
+		T result = super.getOtherEnd();
+		if(result == null) {
+			result = implicitElement();
+		}
+		return result;
+	}
+	
+	/**
+	 * Return the element that is implicitly connected via this association. The
+	 * result should only be non-null when there is no explicitly connected element.
+	 * @return
+	 */
+	public T implicitElement() {
+		return null;
+	}
+
+	/**
+	 * Copy the contents of this association to the given other association.
+	 * @param other
+	 */
+	public void cloneTo(Single<T> other) {
+		T otherEnd = getOtherEnd();
+		if(otherEnd != null) {
+			other.connectTo((Association) otherEnd.clone().parentLink());
+		}
+	}
+	
 }
