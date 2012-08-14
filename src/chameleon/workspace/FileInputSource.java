df92f9018dd1b7e0f63db86f5c251137e12aa8bc
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-08-14 19:33:23 +0200

    added missing files

---
+++ src/chameleon/workspace/FileInputSource.java
@@ -0,0 +1,56 @@
+package chameleon.workspace;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import chameleon.core.document.Document;
+import chameleon.input.ModelFactory;
+import chameleon.input.ParseException;
+
+public abstract class FileInputSource implements InputSource {
+
+	public FileInputSource(File file, ModelFactory factory) {
+		if(file == null) {
+			throw new IllegalArgumentException();
+		}
+		if(factory == null) {
+			throw new IllegalArgumentException();
+		}
+		_file = file;
+		_factory = factory;
+
+	}
+	
+	private File _file;
+	
+	public File file() {
+		return _file;
+	}
+	
+	public ModelFactory modelFactory() {
+		return _factory;
+	}
+
+	private ModelFactory _factory;
+	
+	@Override
+	public void load() throws IOException, ParseException {
+		InputStream fileInputStream;
+	  fileInputStream = new FileInputStream(file());
+	  if(_document == null) {
+		  _document = new Document();
+	  } else {
+	  	_document.disconnect();
+	  }
+		modelFactory().parse(fileInputStream, _document);
+	}
+
+	private Document _document;
+	
+	public Document document() {
+		return _document;
+	}
+	
+}
