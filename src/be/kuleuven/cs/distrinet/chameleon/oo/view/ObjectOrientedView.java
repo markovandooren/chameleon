dc3bb41a56a148339ab5a26c38128b51a574f3e3
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-06-25 11:56:34 +0200

    added missing file

---
+++ src/be/kuleuven/cs/distrinet/chameleon/oo/view/ObjectOrientedView.java
@@ -0,0 +1,25 @@
+package be.kuleuven.cs.distrinet.chameleon.oo.view;
+
+import be.kuleuven.cs.distrinet.chameleon.core.language.Language;
+import be.kuleuven.cs.distrinet.chameleon.core.lookup.LookupException;
+import be.kuleuven.cs.distrinet.chameleon.core.namespace.RootNamespace;
+import be.kuleuven.cs.distrinet.chameleon.oo.language.ObjectOrientedLanguage;
+import be.kuleuven.cs.distrinet.chameleon.oo.type.Type;
+import be.kuleuven.cs.distrinet.chameleon.oo.type.TypeReference;
+import be.kuleuven.cs.distrinet.chameleon.workspace.View;
+
+public abstract class ObjectOrientedView extends View {
+
+	public ObjectOrientedView(RootNamespace namespace, Language language) {
+		super(namespace,language);
+	}
+
+//	public abstract TypeReference createTypeReference(String fqn);
+	
+//	public abstract Type getNullType();
+	
+	public Type findType(String fqn) throws LookupException {
+		return ((ObjectOrientedLanguage)language()).findType(fqn, namespace());
+	}
+
+}
