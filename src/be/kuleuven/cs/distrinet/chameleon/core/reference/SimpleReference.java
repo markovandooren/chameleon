339386e5bf3b41ca0cc25ec63c89d84f502f68f0
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-07-12 11:35:44 +0200

    cleaning up simple reference hierarchy

--- src/be/kuleuven/cs/distrinet/chameleon/core/reference/SimpleReference.java
+++ src/be/kuleuven/cs/distrinet/chameleon/core/reference/SimpleReference.java
@@ -7,58 +7,14 @@ import be.kuleuven.cs.distrinet.chameleon.core.declaration.QualifiedName;
 import be.kuleuven.cs.distrinet.chameleon.core.declaration.Signature;
 import be.kuleuven.cs.distrinet.chameleon.core.declaration.SimpleNameSignature;
 import be.kuleuven.cs.distrinet.chameleon.core.declaration.TargetDeclaration;
-import be.kuleuven.cs.distrinet.chameleon.core.lookup.DeclarationCollector;
 import be.kuleuven.cs.distrinet.chameleon.core.lookup.DeclarationSelector;
-import be.kuleuven.cs.distrinet.chameleon.core.lookup.LookupException;
 import be.kuleuven.cs.distrinet.chameleon.core.lookup.SelectorWithoutOrder;
 import be.kuleuven.cs.distrinet.chameleon.util.Util;
-import be.kuleuven.cs.distrinet.chameleon.util.association.Single;
 
 public class SimpleReference<D extends Declaration> extends ElementReference<D> {
 
 	private Class<D> _specificClass;
 	
-	
-	/*@
-  @ public behavior
-  @
-  @ pre qn != null;
-  @
-  @ post getTarget() == getTarget(Util.getAllButLastPart(qn));
-  @ post getName() == Util.getLastPart(qn);
-  @*/
-	private SimpleReference(String qn) {
-		this(getTarget(Util.getAllButLastPart(qn)), new SimpleNameSignature(Util.getLastPart(qn)));
-	}
-
-	/*@
-  @ public behavior
-  @
-  @ pre name != null;
-  @
-  @ post getTarget() == target;
-  @ post getName() == name;
-  @*/
-	private SimpleReference(CrossReferenceTarget target, Signature signature) {
-		super(signature);
-		setTarget(target); 
-	}
-
-
-
-	public SimpleReference(CrossReferenceTarget target, String name, Class<D> specificClass) {
-		this(target, new SimpleNameSignature(name), specificClass);
-	}
-
-	public SimpleReference(CrossReferenceTarget  target, Signature signature, Class<D> specificClass) {
-		this(target, signature);
-		_specificClass = specificClass;
-	}
-
-	public SimpleReference(QualifiedName name, Class<D> specificClass) {
-		this(targetOf(name, specificClass), Util.clone(name.lastSignature()), specificClass);
-	}
-
 	/**
 	 * Initialize a new simple reference given a fully qualified name. The name is split at every dot, and
 	 * multiple objects are created to form a chain of references.
@@ -66,8 +22,38 @@ public class SimpleReference<D extends Declaration> extends ElementReference<D> 
 	 * @param specificClass
 	 */
 	public SimpleReference(String fqn, Class<D> specificClass) {
-		this(fqn);
+		this(fqn,specificClass,false);
+	}
+
+  public SimpleReference(String fqn, Class<D> specificClass, boolean recursiveLimit) {
+    this(createTarget(fqn, specificClass, recursiveLimit), 
+                   new SimpleNameSignature(Util.getLastPart(fqn)), 
+                   specificClass);
+  }
+
+	protected static SimpleReference createTarget(String fqn, Class specificClass, boolean recursiveLimit) {
+		String allButLastPart = Util.getAllButLastPart(fqn);
+		if(allButLastPart == null) {
+			return null;
+		} else {
+			return new SimpleReference(allButLastPart, recursiveLimit ? specificClass : TargetDeclaration.class, recursiveLimit);
+		}
+	}
+
+  
+	public SimpleReference(CrossReferenceTarget target, String name, Class<D> specificClass) {
+		this(target, new SimpleNameSignature(name), specificClass);
+	}
+
+	public SimpleReference(CrossReferenceTarget  target, Signature signature, Class<D> specificClass) {
+		super(signature);
+		setTarget(target); 
 		_specificClass = specificClass;
+	}
+
+	//Only used in JLo compiler. Need to test if the limit should be recursive or not.
+	public SimpleReference(QualifiedName name, Class<D> specificClass) {
+		this(targetOf(name, specificClass), Util.clone(name.lastSignature()), specificClass);
 	}
 
 	/**
@@ -101,62 +87,6 @@ public class SimpleReference<D extends Declaration> extends ElementReference<D> 
 		return _specificClass;
 	}
 
-	/**
-	 * TARGET
-	 */
-	private Single<CrossReferenceTarget> _target = new Single<CrossReferenceTarget>(this);
-
-	protected Single<CrossReferenceTarget> targetLink() {
-		return _target;
-	}
-
-	public CrossReferenceTarget getTarget() {
-		return _target.getOtherEnd();
-	}
-
-	public void setTarget(CrossReferenceTarget target) {
-		set(_target,target);
-	}
-
-	/*@
-	  @ also public behavior
-	  @
-	  @ post getTarget() == null ==> \result == getContext(this).findPackageOrType(getName());
-	  @ post getTarget() != null ==> (
-	  @     (getTarget().getPackageOrType() == null ==> \result == null) &&
-	  @     (getTarget().getPackageOrType() == null ==> \result == 
-	  @         getTarget().getPackageOrType().getTargetContext().findPackageOrType(getName()));
-	  @*/
-	public <X extends Declaration> X getElement(DeclarationSelector<X> selector) throws LookupException {
-		X result = null;
-
-		//OPTIMISATION
-		boolean cache = selector.equals(selector());
-		if(cache) {
-			result = (X) getCache();
-		}
-		if(result != null) {
-			return result;
-		}
-
-		DeclarationCollector<X> collector = new DeclarationCollector<X>(selector);
-		CrossReferenceTarget targetReference = getTarget();
-		if(targetReference != null) {
-			targetReference.targetContext().lookUp(collector);
-		}
-		else {
-			lexicalContext().lookUp(collector);
-		}
-		result = collector.result();
-		if(cache) {
-			setCache((D) result);
-		}
-		return result;
-	}
-
-	public String toString() {
-		return (getTarget() == null ? "" : getTarget().toString()+".")+signature().toString();
-	}
 
 	public static <DD extends Declaration> CrossReference targetOf(QualifiedName name, Class<DD> specificClass) {
 		SimpleReference current = null;
