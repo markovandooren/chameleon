831223d3da12189a4fd36d102f025a337f6ce3ef
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-07-20 15:19:41 +0200

    frugal document activation

--- src/be/kuleuven/cs/distrinet/chameleon/core/document/Document.java
+++ src/be/kuleuven/cs/distrinet/chameleon/core/document/Document.java
@@ -68,13 +68,16 @@ public class Document extends ElementImpl {
 	}
 	
 	/**
-	 * Activate this document by letting all descendant namespace declarations
-	 * connect themselves to their corresponding namespace. This adds the contents
+	 * Activate this document by letting all child namespace declarations
+	 * activate themselves. This adds the contents
 	 * of this document to the logical namespace structure of the project. 
 	 */
 	public void activate() {
-		for(NamespaceDeclaration nsd: descendants(NamespaceDeclaration.class)) {
-			nsd.namespace();
+//		for(NamespaceDeclaration nsd: descendants(NamespaceDeclaration.class)) {
+//			nsd.namespace();
+//		}
+		for(NamespaceDeclaration part: namespaceDeclarations()) {
+			part.activate();
 		}
 	}
 	
