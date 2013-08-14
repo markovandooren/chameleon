820cd3376c26d3f12bbc9eaa64063565463d620b
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-08-14 14:16:26 +0200

    testing single element always returned false

--- src/be/kuleuven/cs/distrinet/chameleon/eclipse/property/ChameleonPropertyTester.java
+++ src/be/kuleuven/cs/distrinet/chameleon/eclipse/property/ChameleonPropertyTester.java
@@ -49,7 +49,7 @@ public class ChameleonPropertyTester extends PropertyTester {
 		if(! result) {
 			result = testObject(receiver, property, args, expectedValue);
 		}
-		return false;
+		return result;
 	}
 	
 	/**
