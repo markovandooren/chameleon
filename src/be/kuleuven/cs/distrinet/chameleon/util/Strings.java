f48bdb364e1eb668ecd4e4cefbc39b2a868ec542
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-08-15 23:38:33 +0200

    new util classes

---
+++ src/be/kuleuven/cs/distrinet/chameleon/util/Strings.java
@@ -0,0 +1,36 @@
+package be.kuleuven.cs.distrinet.chameleon.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Strings {
+
+	public static String create(Collection<?> collection) {
+		return make(collection);
+	}
+	
+	private static String make(Object object) {
+		if(object instanceof Collection) {
+			return doCreate((Collection<?>) object);
+		} else if(object != null){
+			return object.toString();
+		} else {
+			return "null";
+		}
+	}
+	
+	private static String doCreate(Collection<?> collection) {
+		StringBuilder builder = new StringBuilder();
+		builder.append('[');
+		Iterator<?> iterator = collection.iterator();
+		while(iterator.hasNext()) {
+			builder.append(make(iterator.next()));
+			if(iterator.hasNext()) {
+				builder.append(", ");
+			}
+		}
+		builder.append(']');
+		return builder.toString();
+	}
+
+}
