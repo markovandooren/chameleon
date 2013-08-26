d67f72ae48cd733197c1722c15074a3db6f82bf6
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-08-26 18:22:07 +0200

    add combo box

--- src/be/kuleuven/cs/distrinet/chameleon/ui/widget/WidgetFactory.java
+++ src/be/kuleuven/cs/distrinet/chameleon/ui/widget/WidgetFactory.java
@@ -1,5 +1,12 @@
 package be.kuleuven.cs.distrinet.chameleon.ui.widget;
 
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.checkbox.CheckboxListener;
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.list.ComboBoxController;
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.list.ComboBoxListener;
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.list.ListContentProvider;
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.tree.TreeContentProvider;
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.tree.TreeListener;
+
 
 
 public interface WidgetFactory<W> {
@@ -7,4 +14,6 @@ public interface WidgetFactory<W> {
 	public SelectionController<? extends W> createCheckbox(String text, boolean initialState, CheckboxListener listener);
 	
 	public <V> SelectionController<? extends W> createTristateTree(TreeContentProvider<V> contentProvider, LabelProvider provider, TreeListener<V> listener);
+	
+	public <V> ComboBoxController<? extends W> createComboBox(ListContentProvider<V> contentProvider, LabelProvider provider, ComboBoxListener listener, int baseOneDefaultSelection);
 }
