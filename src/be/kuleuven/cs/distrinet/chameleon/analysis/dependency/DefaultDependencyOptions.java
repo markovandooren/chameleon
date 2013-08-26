09282f248c92aad6e4dbfd42b60d857311bc3c15
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-08-26 18:18:43 +0200

    add the mapper

--- src/be/kuleuven/cs/distrinet/chameleon/analysis/dependency/DefaultDependencyOptions.java
+++ src/be/kuleuven/cs/distrinet/chameleon/analysis/dependency/DefaultDependencyOptions.java
@@ -8,8 +8,9 @@ import be.kuleuven.cs.distrinet.chameleon.analysis.predicate.IsSource;
 import be.kuleuven.cs.distrinet.chameleon.core.element.Element;
 import be.kuleuven.cs.distrinet.chameleon.eclipse.view.dependency.DependencyConfiguration;
 import be.kuleuven.cs.distrinet.chameleon.plugin.LanguagePluginImpl;
-import be.kuleuven.cs.distrinet.chameleon.ui.widget.CheckboxSelector;
 import be.kuleuven.cs.distrinet.chameleon.ui.widget.PredicateSelector;
+import be.kuleuven.cs.distrinet.chameleon.ui.widget.checkbox.CheckboxSelector;
+import be.kuleuven.cs.distrinet.rejuse.function.Function;
 
 public class DefaultDependencyOptions extends LanguagePluginImpl implements DependencyOptions {
 
@@ -17,8 +18,15 @@ public class DefaultDependencyOptions extends LanguagePluginImpl implements Depe
 	public DependencyConfiguration createConfiguration() {
 		List<PredicateSelector<? super Element>> source = new ArrayList<>();
 		List<PredicateSelector<? super Element>> target = new ArrayList<>();
+		Function identity = new Function() {
+
+			@Override
+			public Object apply(Object argument) throws Exception {
+				return argument;
+			}
+		};
 		target.add(onlySource());
-		return new DependencyConfiguration(source, Collections.EMPTY_LIST,target,Collections.EMPTY_LIST);
+		return new DependencyConfiguration(source, Collections.EMPTY_LIST,target,Collections.EMPTY_LIST, identity);
 	}
 
 	public CheckboxSelector<Element> onlySource() {
