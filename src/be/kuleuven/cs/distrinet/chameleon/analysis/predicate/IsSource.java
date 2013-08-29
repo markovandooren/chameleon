57ce9167f56e7d54bcf5d93b23504ec376e86620
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-08-30 00:37:06 +0200

    match namespaces loaded by binary and source loaders

--- src/be/kuleuven/cs/distrinet/chameleon/analysis/predicate/IsSource.java
+++ src/be/kuleuven/cs/distrinet/chameleon/analysis/predicate/IsSource.java
@@ -1,6 +1,11 @@
 package be.kuleuven.cs.distrinet.chameleon.analysis.predicate;
 
+import java.util.List;
+
 import be.kuleuven.cs.distrinet.chameleon.core.element.Element;
+import be.kuleuven.cs.distrinet.chameleon.core.namespace.Namespace;
+import be.kuleuven.cs.distrinet.chameleon.workspace.DocumentLoader;
+import be.kuleuven.cs.distrinet.chameleon.workspace.View;
 import be.kuleuven.cs.distrinet.rejuse.action.Nothing;
 import be.kuleuven.cs.distrinet.rejuse.predicate.UniversalPredicate;
 
@@ -12,7 +17,18 @@ public class IsSource extends UniversalPredicate<Element,Nothing> {
 
 	@Override
 	public boolean uncheckedEval(Element element) throws Nothing {
-		return element.view().isSource(element);
+		View view = element.view();
+		if(element instanceof Namespace) {
+			List<DocumentLoader> sourceLoaders = view.sourceLoaders();
+			for(DocumentLoader loader:sourceLoaders) {
+				if(loader.namespaces().contains(element)) {
+					return true;
+				}
+			}
+			return false;
+		} else {
+			return view.isSource(element);
+		}
 	} 
 
 }
