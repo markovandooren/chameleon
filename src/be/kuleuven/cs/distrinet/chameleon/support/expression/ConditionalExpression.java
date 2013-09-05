b6a1c69ccb5afb391b6f48bcb54cad9e7f91707c
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-09-05 11:20:22 +0200

    use least upper bound for reference types with no subtypeOf relation

--- src/be/kuleuven/cs/distrinet/chameleon/support/expression/ConditionalExpression.java
+++ src/be/kuleuven/cs/distrinet/chameleon/support/expression/ConditionalExpression.java
@@ -3,10 +3,14 @@ package be.kuleuven.cs.distrinet.chameleon.support.expression;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.google.common.collect.ImmutableList;
+
 import be.kuleuven.cs.distrinet.chameleon.core.lookup.LookupException;
 import be.kuleuven.cs.distrinet.chameleon.exception.ChameleonProgrammerException;
 import be.kuleuven.cs.distrinet.chameleon.oo.expression.Expression;
+import be.kuleuven.cs.distrinet.chameleon.oo.language.ObjectOrientedLanguage;
 import be.kuleuven.cs.distrinet.chameleon.oo.type.Type;
+import be.kuleuven.cs.distrinet.chameleon.oo.type.TypeReference;
 
 /**
  * @author Marko van Dooren
@@ -51,14 +55,20 @@ public class ConditionalExpression extends TernaryExpression {
     }
     else if (secondType.assignableTo(firstType)) {
       return firstType;
-    }
-//    else if ((firstType.getName().equals("short") && secondType.getName().equals("byte")) || (firstType.getName().equals("byte") && secondType.getName().equals("short"))) {
-//      return getNamespace().getDefaultNamespace().findType("short");
-//    }
-
-    else {
-      throw new ChameleonProgrammerException("Numerical promotion in conditional expression not yet complete");
-    }
+    } else {
+			ObjectOrientedLanguage language = language(ObjectOrientedLanguage.class);
+			if (firstType.isTrue(language.REFERENCE_TYPE) && secondType.isTrue(language.REFERENCE_TYPE)) {
+				TypeReference first = language.reference(firstType);
+				TypeReference second = language.reference(secondType);
+				return language.subtypeRelation().leastUpperBound(ImmutableList.of(first,second));
+			}
+			else {
+			  throw new ChameleonProgrammerException("Numerical promotion in conditional expression not yet complete");
+			}
+		}
+//  else if ((firstType.getName().equals("short") && secondType.getName().equals("byte")) || (firstType.getName().equals("byte") && secondType.getName().equals("short"))) {
+//  return getNamespace().getDefaultNamespace().findType("short");
+//}
   }
 
   protected ConditionalExpression cloneSelf() {
