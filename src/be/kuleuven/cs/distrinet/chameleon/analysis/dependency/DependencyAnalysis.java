834a83a132743ca56ff4451f425988f3066d02c1
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-09-07 00:02:30 +0200

    don't create ancestor finder predicate in each doPerform execution

--- src/be/kuleuven/cs/distrinet/chameleon/analysis/dependency/DependencyAnalysis.java
+++ src/be/kuleuven/cs/distrinet/chameleon/analysis/dependency/DependencyAnalysis.java
@@ -1,11 +1,15 @@
 package be.kuleuven.cs.distrinet.chameleon.analysis.dependency;
 
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
 
 import be.kuleuven.cs.distrinet.chameleon.analysis.Analysis;
 import be.kuleuven.cs.distrinet.chameleon.core.declaration.Declaration;
 import be.kuleuven.cs.distrinet.chameleon.core.element.Element;
 import be.kuleuven.cs.distrinet.chameleon.core.lookup.LookupException;
+import be.kuleuven.cs.distrinet.chameleon.core.namespace.RootNamespace;
 import be.kuleuven.cs.distrinet.chameleon.core.reference.CrossReference;
 import be.kuleuven.cs.distrinet.rejuse.action.Nothing;
 import be.kuleuven.cs.distrinet.rejuse.contract.Contracts;
@@ -52,7 +56,7 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
       Function<D,D,Nothing> declarationMapper,
 			UniversalPredicate<D,Nothing> declarationPredicate, 
 			UniversalPredicate<? super Dependency<? super E, ? super CrossReference, ? super D>,Nothing> dependencyPredicate,
-			HistoryFilter<E> historyFilter) {
+			HistoryFilter<E,D> historyFilter) {
 		this(elementPredicate.type(), 
 				 elementPredicate, 
 				 crossReferencePredicate,
@@ -70,7 +74,7 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 			                      Function<D,D,Nothing> declarationMapper,
 														UniversalPredicate<? super D,Nothing> declarationPredicate, 
 														UniversalPredicate<? super Dependency<? super E, ? super CrossReference, ? super D>,Nothing> dependencyPredicate,
-														HistoryFilter<E> historyFilter) {
+														HistoryFilter<E,D> historyFilter) {
 		super(Element.class, new DependencyResult());
 		Contracts.notNull(elementType, "The element type should not be null");
 		Contracts.notNull(elementPredicate, "The element predicate should not be null");
@@ -87,6 +91,13 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 		_elementType = elementType;
 		_declarationType = declarationType;
 		_historyFilter = historyFilter;
+		_dependencyFinder = new UniversalPredicate<D,Nothing>(_declarationType) {
+			@Override
+			public boolean uncheckedEval(D t) throws Nothing {
+				return _declarationPredicate.eval(_declarationMapper.apply(t));
+			}
+		};
+
 	}
 	
 	private UniversalPredicate<Dependency<? super E,? super CrossReference,? super D>,Nothing> _noSelfReference = (UniversalPredicate)new UniversalPredicate<Dependency, Nothing>(Dependency.class) {
@@ -96,23 +107,27 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 		}
 	};
 	
-	public abstract static class HistoryFilter<E> {
+	public static class HistoryFilter<E extends Element, D extends Declaration> {
 		
-		public abstract boolean process(Dependency<E, CrossReference, E> dependency, DependencyResult result);
-	}
-	
-	public static class NOOP<E> extends HistoryFilter<E> {
-
-		@Override
-		public boolean process(Dependency<E, CrossReference, E> dependency, DependencyResult result) {
+		public boolean process(Dependency<E, CrossReference, D> dependency, DependencyResult result) {
 			return true;
 		}
+		
+		public boolean processSource(E source, DependencyResult result) {
+			return true;
+		}
+	}
+	
+	public static class NOOP<E extends Element, D extends Declaration> extends HistoryFilter<E,D> {
+
 	}
 	
 	private Class<D> _declarationType;
 
 	private Class<? extends E> _elementType;
 
+	private final UniversalPredicate<D,Nothing> _dependencyFinder;
+	
 	private final UniversalPredicate<?,Nothing> _elementPredicate;
 	
 	private final UniversalPredicate<? super D,Nothing> _declarationPredicate;
@@ -129,7 +144,7 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 	
 	private final Function<D, D,Nothing> _declarationMapper;
 
-	private HistoryFilter<E> _historyFilter;
+	private HistoryFilter<E,D> _historyFilter;
 	
 	@Override
 	public void doEnter(Element object) {
@@ -145,7 +160,12 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 				_elements.removeLast();
 			}
 		}
+//		if(object instanceof RootNamespace) {
+//			System.out.println(_stopwatch.elapsedTime(TimeUnit.MILLISECONDS));
+//		}
 	}
+	
+//	private Stopwatch _stopwatch = new Stopwatch();
 	
 	private LinkedList<Element> _elements = new LinkedList<>();
 	
@@ -154,27 +174,28 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 	protected void doPerform(final Element element) throws Nothing {
 		try {
 			if(! _elements.isEmpty()) {
-				if(_crossReferencePredicate.eval(element)) {
+				boolean crossReferenceMatches = _crossReferencePredicate.eval(element);
+				if(crossReferenceMatches) {
 					CrossReference<?> cref = (CrossReference<?>) element;
+//					_stopwatch.start();
+					// This is the expensive one. File loading is relatively large part 
+					// in the first run.
 					Declaration decl = cref.getElement();
+//					_stopwatch.stop();
 
-					UniversalPredicate<D,Nothing> predicate = new UniversalPredicate<D,Nothing>(_declarationType) {
-						@Override
-						public boolean uncheckedEval(D t) throws Nothing {
-							return _declarationPredicate.eval(_declarationMapper.apply(t));
-						}
-					};
-					D container = decl.logical().nearestAncestorOrSelf(decl,predicate);
-					if(container != null) {
+					D unmappedDependee = decl.logical().nearestAncestorOrSelf(decl,_dependencyFinder);
+					if(unmappedDependee != null) {
 						for(Element e: _elements) {
 							// SLOW mapping is applied twice, but that should be peanuts compared
 							// to the rest of the analysis.
-							D apply = _declarationMapper.apply(container);
-							Dependency dependency = new Dependency(e,cref,apply);
+							// The search looks for an ancestor for which the mapping matches. Therefore
+							// it must be mapped again afterwards.
+							D dependee = _declarationMapper.apply(unmappedDependee);
+							Dependency dependency = new Dependency(e,cref,dependee);
 							if(_dependencyPredicate.eval(dependency)) {
 								DependencyResult result = result();
 								if(_historyFilter.process(dependency, result)) {
-									result.add(e,apply);
+									result.add(dependency);
 								}
 							}
 						}
@@ -182,6 +203,9 @@ public class DependencyAnalysis<E extends Element, D extends Declaration> extend
 				}
 			}
 		} catch (LookupException e) {
+//			if(_stopwatch.isRunning()) {
+//				_stopwatch.stop();
+//			}
 			// No edge is added when we cannot determine the dependency
 			e.printStackTrace();
 		}
