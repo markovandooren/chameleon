1eb889c549a0be46c17a019dee0fd35e1cbafab1
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-09-18 23:33:59 +0200

    sometimes the selection is a project nature instead of a project. adapt accordingly

--- src/be/kuleuven/cs/distrinet/chameleon/eclipse/nature/AddBackgroundNature.java
+++ src/be/kuleuven/cs/distrinet/chameleon/eclipse/nature/AddBackgroundNature.java
@@ -5,11 +5,14 @@ import java.util.Iterator;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.internal.resources.ProjectNatureDescriptor;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.internal.core.JavaProject;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -34,6 +37,9 @@ public class AddBackgroundNature extends AbstractHandler {
 			while(iterator.hasNext()) {
 				try {
 				Object current = iterator.next();
+				if(current instanceof IProjectNature) {
+					current = ((IProjectNature) current).getProject();
+				}
 				if(current instanceof IProject) {
 					IProject project = (IProject) current;
 					if(project.exists() && project.isOpen()) {
@@ -54,6 +60,8 @@ public class AddBackgroundNature extends AbstractHandler {
 							Files.copy(backup, projectFile);
 						}
 					}
+				} else {
+					System.out.println(current.getClass());
 				}
 				} catch(CoreException exc) {
 					// Because of the check for project.exists() and project.isOpen()
