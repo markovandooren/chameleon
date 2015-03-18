6f8ec6c5517616c0a361a01407fd4f81e0b0d0fa
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-03-18 01:11:56 +0100

    formatting

--- src/org/aikodi/chameleon/core/namespace/DocumentLoaderNamespace.java
+++ src/org/aikodi/chameleon/core/namespace/DocumentLoaderNamespace.java
@@ -6,25 +6,27 @@ import org.aikodi.chameleon.workspace.DocumentLoader;
 import org.aikodi.chameleon.workspace.InputException;
 
 /**
- * An interface for namespaces that cooperate with document loaders
- * to enable lazy loading of documents.
+ * An interface for namespaces that cooperate with document loaders to enable
+ * lazy loading of documents.
  * 
  * @author Marko van Dooren
  */
 public interface DocumentLoaderNamespace extends Namespace {
 
-   /**
-    * Add the given document loader.
-    * 
-    * @param loader The document loader to be added.
-    * @throws InputException The document loader could not be added because
-    *                        an exception was thrown while asking the loader
-    *                        for the names of the top-level declarations.
-    */
-	public void addDocumentLoader(DocumentLoader loader) throws InputException;
-	
-	/**
-	 * @return the document loaders that load documents into this namespace.
-	 */
-	public List<DocumentLoader> documentLoaders();
+  /**
+   * Add the given document loader.
+   * 
+   * @param loader
+   *          The document loader to be added.
+   * @throws InputException
+   *           The document loader could not be added because an exception was
+   *           thrown while asking the loader for the names of the top-level
+   *           declarations.
+   */
+  public void addDocumentLoader(DocumentLoader loader) throws InputException;
+
+  /**
+   * @return the document loaders that load documents into this namespace.
+   */
+  public List<DocumentLoader> documentLoaders();
 }
