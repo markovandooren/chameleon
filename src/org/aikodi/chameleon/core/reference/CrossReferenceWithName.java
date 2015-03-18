52bde8d318b3fd9ab9924408f2d1183524974dd4
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-03-18 01:11:16 +0100

    Added some documentation

--- src/org/aikodi/chameleon/core/reference/CrossReferenceWithName.java
+++ src/org/aikodi/chameleon/core/reference/CrossReferenceWithName.java
@@ -2,11 +2,25 @@ package org.aikodi.chameleon.core.reference;
 
 import org.aikodi.chameleon.core.declaration.Declaration;
 
+/**
+ * <p>A cross-reference that contains the name of a declaration. Examples
+ * are references to types and variables, or method calls.</p>
+ *  
+ * @author Marko van Dooren
+ *
+ * @param <D> The type of the referenced declaration.
+ */
 public interface CrossReferenceWithName<D extends Declaration> extends CrossReference<D> {
 
+  /**
+   * Change the name that is used during lookup of the referenced element.
+   * @param name The new name that is used during lookup of the referenced element.
+   */
 	public void setName(String name);
 	
+	/**
+	 * @return the name of the referenced element.
+	 */
 	public String name();
-	
 
 }
