f57e28750a9dc808c1c62f851f460c312401b259
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-03-27 01:23:34 +0100

    make event useful

--- src/org/aikodi/chameleon/core/element/Added.java
+++ src/org/aikodi/chameleon/core/element/Added.java
@@ -1,6 +1,9 @@
 package org.aikodi.chameleon.core.element;
 
-public class Added extends Change {
+public class Added extends ChildChanged {
 
+  public Added(Element element) {
+    super(element);
+  }
 
 }
