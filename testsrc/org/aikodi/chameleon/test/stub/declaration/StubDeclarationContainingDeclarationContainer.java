27e123bdd85461d7255e69ca80ae459228a2e0fd
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-04-02 17:30:08 +0200

    move field down to remove super class

--- testsrc/org/aikodi/chameleon/test/stub/declaration/StubDeclarationContainingDeclarationContainer.java
+++ testsrc/org/aikodi/chameleon/test/stub/declaration/StubDeclarationContainingDeclarationContainer.java
@@ -1,17 +1,28 @@
 package org.aikodi.chameleon.test.stub.declaration;
 
-import org.aikodi.chameleon.core.declaration.CommonDeclarationContainingDeclaration;
+import java.util.List;
+
 import org.aikodi.chameleon.core.declaration.Declaration;
-import org.aikodi.chameleon.core.declaration.Signature;
+import org.aikodi.chameleon.core.declaration.DeclarationContainingDeclarationImpl;
 import org.aikodi.chameleon.core.declaration.SimpleNameSignature;
 import org.aikodi.chameleon.core.element.Element;
-import org.aikodi.chameleon.core.lookup.LookupException;
-import org.aikodi.chameleon.core.lookup.SelectionResult;
+import org.aikodi.chameleon.util.association.Multi;
 
-public class StubDeclarationContainingDeclarationContainer extends CommonDeclarationContainingDeclaration {
+public class StubDeclarationContainingDeclarationContainer extends DeclarationContainingDeclarationImpl {
+
+  private Multi<Declaration> _declarations = new Multi<Declaration>(this);
+  
+  public void addDeclaration(Declaration d) {
+    add(_declarations,d);
+  }
+
+  public List<? extends Declaration> locallyDeclaredDeclarations() {
+    return _declarations.getOtherEnds();
+  }
+  
 
   protected StubDeclarationContainingDeclarationContainer() {
-    super();
+    super(null);
   }
 
   public StubDeclarationContainingDeclarationContainer(String name) {
