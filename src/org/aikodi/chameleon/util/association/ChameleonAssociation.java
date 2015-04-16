169745c3d52d7c5eee64a7b3e194bc2ce63054f5
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-04-16 15:45:36 +0200

    Adding documentation

--- src/org/aikodi/chameleon/util/association/ChameleonAssociation.java
+++ src/org/aikodi/chameleon/util/association/ChameleonAssociation.java
@@ -8,16 +8,31 @@ import org.aikodi.chameleon.core.validation.Verification;
 
 import be.kuleuven.cs.distrinet.rejuse.association.IAssociation;
 
+/**
+ * A interface for lexical associations used for Chameleon models.
+ * 
+ * @author Marko van Dooren
+ *
+ * @param <T> The type of element at the other end of the association.
+ */
 public interface ChameleonAssociation<T extends Element> extends IAssociation<Element, T> {
 
-//	public void cloneTo(ChameleonAssociation<T> o);
-	
+  /**
+   * Verify whether the constraints for this association end are satisfied.
+   * 
+   * @return Verification.valid() if the constraints are satisfied. Otherwise
+   * an object representing the problems.
+   */
 	public Verification verify();
 	
+	/**
+	 * @return A string that describes the role of the elements at the
+	 * other end of the association.
+	 */
 	public String role();
 	
 	public void pairWise(ChameleonAssociation<?> other, BiConsumer<Element, Element> consumer);
 	
-   public void mapTo(ChameleonAssociation<?> other, Function<Element, Element> mapper);
+  public void mapTo(ChameleonAssociation<?> other, Function<Element, Element> mapper);
 
 }
