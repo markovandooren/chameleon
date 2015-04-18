85ae4f5f4502f0e913c481b1745579d169b41770
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-04-18 22:16:31 +0200

    reformatting documentation

--- src/org/aikodi/chameleon/oo/expression/Expression.java
+++ src/org/aikodi/chameleon/oo/expression/Expression.java
@@ -12,17 +12,30 @@ import org.aikodi.chameleon.core.reference.CrossReferenceTarget;
 import org.aikodi.chameleon.oo.type.Type;
 
 /**
- * A class of elements representing expressions in a language. Each expression has a type.
+ * A class of elements representing expressions in a language. 
+ * Each expression has a type.
  *  
  * @author Marko van Dooren
  */
 
 public abstract class Expression extends ElementImpl implements CrossReferenceTarget {
 
+  /**
+   * The type of an expression is cached to increase performance. Call {@link #flushCache()} to
+   * flush the cache of the model has changed.
+   * 
+   * The reference is stored in a soft reference to allow garbage collection of cached types.
+   * 
+   * The soft reference is stored in an atomic reference to deal with concurrent lookups of the
+   * type of this expression without needing a lock.
+   */
+  private final AtomicReference<SoftReference<Type>> _cache = new AtomicReference<>();
+  
 	/**
-	 * Return the type of this expression. The actual computation of the type is done in actualType. This
-	 * method performs the caching. Enable or disable caching of expression types using Config.CACHE_EXPRESSION_TYPES.
-	 */
+   * Return the type of this expression. The actual computation of the type is
+   * done in {@link #actualType()}. This method performs the caching. Enable or disable
+   * caching of expression types using Config.CACHE_EXPRESSION_TYPES.
+   */
  /*@
    @ public behavior
    @
@@ -46,33 +59,28 @@ public abstract class Expression extends ElementImpl implements CrossReferenceTa
 	@Override
 	public void flushLocalCache() {
 		super.flushLocalCache();
-//		_typeCache = null;
 		boolean success = false;
 		do {
 			success = _cache.compareAndSet(_cache.get(), null);
 		} while(! success);
-
 	}
 	
 	/**
-	 * The type of an expression is cached to increase performance. Call {@link #flushCache()} to
-	 * flush the cache of the model has changed.
+	 * Compute the type of this expression
 	 * 
-	 * The reference is stored in a soft reference to allow garbage collection of cached types.
+	 * @return the type of this expression
 	 * 
-	 * The soft reference is stored in an atomic reference to deal with concurrent lookups of the
-	 * type of this expression without needing a lock.
+	 * @throws LookupException the type could not be computed because of an
+	 * exception encountered during the lookup of a declaration.
 	 */
-	private final AtomicReference<SoftReference<Type>> _cache = new AtomicReference<>();
-	
 	protected abstract Type actualType() throws LookupException;
 
 	/**
 	 * @see {@link Target#targetContext()}
 	 */
-  @Override
-public LocalLookupContext<?> targetContext() throws LookupException {
-    return getType().targetContext();
-  }
+	@Override
+	public LocalLookupContext<?> targetContext() throws LookupException {
+	  return getType().targetContext();
+	}
 
 }
