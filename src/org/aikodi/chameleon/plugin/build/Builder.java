1d2ff0ec0f9b7d2ba15521f371f74e2bcc999ed5
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-04-18 22:14:57 +0200

    adding documentation

--- src/org/aikodi/chameleon/plugin/build/Builder.java
+++ src/org/aikodi/chameleon/plugin/build/Builder.java
@@ -6,8 +6,36 @@ import java.util.List;
 import org.aikodi.chameleon.core.document.Document;
 import org.aikodi.chameleon.plugin.ViewPlugin;
 
+/**
+ * A builder translates documents to some output format.
+ * 
+ * @author Marko van Dooren
+ */
 public interface Builder extends ViewPlugin {
 
-	public void build(List<Document> compilationUnits, File outputDir, BuildProgressHelper buildProgressHelper) throws BuildException;
-	public int totalAmountOfWork(List<Document> compilationUnits, List<Document> allProjectCompilationUnits);
+  /**
+   * Build the given documents and write the output in the given
+   * output directory.
+   * 
+   * FIXME: Get rid of the File dependency. It doesn't belong here.
+   * 
+   * @param documents The documents that will be built.
+   * @param outputDir The directory in which the output must be written.
+   * @param buildProgressHelper An object to indicate the progress of the build.
+   * 
+   * @throws BuildException An exception occurred while building.
+   */
+	public void build(List<Document> documents, File outputDir, BuildProgressHelper buildProgressHelper) throws BuildException;
+	
+	/**
+	 * Return an indication for the total amount of work to be done when building
+	 * the given collection of documents.
+	 * 
+	 * @param documents The documents that should be built.
+	 * @param allDocuments The list of all documents.
+	 * 
+	 * @return A number that indicates how much work must be done to build the
+	 * given collection of documents.
+	 */
+	public int totalAmountOfWork(List<Document> documents, List<Document> allDocuments);
 }
