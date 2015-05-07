29fd1931f2c308a96526041b74ede1e9b0a5a3b6
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-07 18:50:30 +0200

    super target is a cross-reference now

--- src/org/aikodi/chameleon/support/expression/SuperTarget.java
+++ src/org/aikodi/chameleon/support/expression/SuperTarget.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import org.aikodi.chameleon.core.declaration.Declaration;
 import org.aikodi.chameleon.core.element.ElementImpl;
-import org.aikodi.chameleon.core.lookup.LookupContext;
 import org.aikodi.chameleon.core.lookup.LookupException;
+import org.aikodi.chameleon.core.reference.CrossReference;
 import org.aikodi.chameleon.core.reference.CrossReferenceTarget;
 import org.aikodi.chameleon.core.validation.Valid;
 import org.aikodi.chameleon.core.validation.Verification;
 import org.aikodi.chameleon.oo.expression.NamedTarget;
-import org.aikodi.chameleon.oo.statement.CheckedExceptionList;
 import org.aikodi.chameleon.oo.type.Type;
 import org.aikodi.chameleon.oo.type.inheritance.InheritanceRelation;
 import org.aikodi.chameleon.util.association.Single;
@@ -18,65 +17,54 @@ import org.aikodi.chameleon.util.association.Single;
 /**
  * @author Marko van Dooren
  */
-public class SuperTarget extends ElementImpl implements CrossReferenceTarget {
+public class SuperTarget extends ElementImpl implements CrossReference<Declaration> {
 
   public SuperTarget() {
-	}
-  
-  public SuperTarget(CrossReferenceTarget target) {
-  	setTarget(target);
   }
 
-	/**
-	 * TARGET
-	 */
-	private Single<CrossReferenceTarget> _target = new Single<CrossReferenceTarget>(this,false);
+  public SuperTarget(CrossReferenceTarget target) {
+    setTarget(target);
+  }
+
+  /**
+   * TARGET
+   */
+  private Single<CrossReferenceTarget> _target = new Single<CrossReferenceTarget>(this, false);
 
   public CrossReferenceTarget getTarget() {
     return _target.getOtherEnd();
   }
 
   public void setTarget(CrossReferenceTarget target) {
-    set(_target,target);
+    set(_target, target);
   }
 
   @Override
-public SuperTarget cloneSelf() {
+  public SuperTarget cloneSelf() {
     return new SuperTarget();
   }
 
-  public CheckedExceptionList getCEL() {
-    return new CheckedExceptionList();
-  }
-
-  public CheckedExceptionList getAbsCEL() {
-    return new CheckedExceptionList();
-  }
-
-  public Declaration getTargetDeclaration() throws LookupException {
-    if(getTarget() != null) {
-      return ((NamedTarget)getTarget()).getElement();
+  @Override
+  public Declaration getElement() throws LookupException {
+    if (getTarget() != null) {
+      return ((NamedTarget) getTarget()).getElement();
     } else {
       Type outer = nearestAncestor(Type.class);
       List<InheritanceRelation> inh = outer.inheritanceRelations();
       Type result = (Type) inh.get(0).superElement();
-			return result;
+      return result;
     }
   }
 
-  @Override
-public LookupContext targetContext() throws LookupException {
-    return getTargetDeclaration().targetContext();
-  }
-
   /**
-   * A super target is always valid. If invocations on a super target must always resolve to an effective declaration, 
-   * as is the case in Java, then the language must add that rule. For mixins, for example, that must only be the case for
-   * an actual combination of mixins.
+   * A super target is always valid. If invocations on a super target must
+   * always resolve to an effective declaration, as is the case in Java, then
+   * the language must add that rule. For mixins, for example, that must only be
+   * the case for an actual combination of mixins.
    */
-	@Override
-	public Verification verifySelf() {
-		return Valid.create();
-	}
+  @Override
+  public Verification verifySelf() {
+    return Valid.create();
+  }
 
 }
