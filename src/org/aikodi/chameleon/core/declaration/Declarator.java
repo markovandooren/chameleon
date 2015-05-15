a5e7defdac7e344f4a43f086bf193cc8f64859ff
Marko van Dooren <Marko.van.Dooren@gmail.com>
2015-05-15 16:11:35 +0200

    Added documentation

--- src/org/aikodi/chameleon/core/declaration/Declarator.java
+++ src/org/aikodi/chameleon/core/declaration/Declarator.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.aikodi.chameleon.util.Lists;
 
 /**
- * A declarator is an element that introduces declarations.
+ * A declarator is an element that declares declarations.
  * 
  * @author Marko van Dooren
  */
 public interface Declarator {
 
-	public default List<Declaration> declarations() {
-		return Lists.create();
-	}
+	/**
+	 * Return the declarations declared by this declarator.
+	 * 
+	 * @return The list of declarations declared by this declarator.
+	 *         The list is not null and does not contain null.
+	 */
+	public List<Declaration> declaredDeclarations();
 }
