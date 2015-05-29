db7c3fb0548279de903184f24e7c93656e291319
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-29 09:48:05 +0200

    Allow taking a limited snapshot of the full trace

--- src/org/aikodi/chameleon/util/CreationStackTrace.java
+++ src/org/aikodi/chameleon/util/CreationStackTrace.java
@@ -11,18 +11,25 @@ import java.util.List;
  */
 public class CreationStackTrace {
 
-	private StackTraceElement[] _stackTrace;
+	private List<StackTraceElement> _stackTrace;
 
 	public List<StackTraceElement> trace() {
-		return new ArrayList<StackTraceElement>(Arrays.asList(_stackTrace));
+		return new ArrayList<StackTraceElement>(_stackTrace);
 	}
 	
 	public CreationStackTrace() {
 		try {
 			throw new Exception();
 		} catch (Exception e) {
-			_stackTrace = e.getStackTrace();
+			_stackTrace = Arrays.asList(e.getStackTrace());
 		}
 	}
 	
+  public CreationStackTrace(int size) {
+    try {
+      throw new Exception();
+    } catch (Exception e) {
+      _stackTrace = Arrays.asList(e.getStackTrace()).subList(0, size);
+    }
+  }
 }
