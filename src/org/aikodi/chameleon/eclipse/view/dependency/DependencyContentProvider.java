04ac17f92440aad206c84d6e4ded39be50f99bca
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-06-10 15:01:23 +0200

    re-enabled dependency view

--- src/org/aikodi/chameleon/eclipse/view/dependency/DependencyContentProvider.java
+++ src/org/aikodi/chameleon/eclipse/view/dependency/DependencyContentProvider.java
@@ -1,44 +1,68 @@
 package org.aikodi.chameleon.eclipse.view.dependency;
-//package be.kuleuven.cs.distrinet.chameleon.eclipse.view.dependency;
-//
-//import org.eclipse.gef4.zest.core.viewers.IGraphContentProvider;
-//import org.eclipse.jface.viewers.Viewer;
-//
-//import be.kuleuven.cs.distrinet.chameleon.analysis.dependency.DependencyResult;
-//import be.kuleuven.cs.distrinet.rejuse.graph.Node;
-//import be.kuleuven.cs.distrinet.rejuse.graph.UniEdge;
-//
-//public class DependencyContentProvider implements IGraphContentProvider {
-//
-//	@Override
-//	public void dispose() {
-//		_result = null;
-//	}
-//
-//	@Override
-//	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-//		_result = (DependencyResult) newInput;
-//	}
-//	
-//	private DependencyResult _result;
-//
-//	@Override
-//	public Node getSource(Object rel) {
-//		return ((UniEdge)rel).startNode();
-//	}
-//
-//	@Override
-//	public Node getDestination(Object rel) {
-//		return ((UniEdge)rel).endNode();
-//	}
-//
-//	@Override
-//	public Object[] getElements(Object input) {
-//		if(_result != null) {
-//			return _result.dependencies().toArray();
-//		} else {
-//			return new Object[0];
-//		}
-//	}
-//
-//}
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.aikodi.chameleon.analysis.dependency.DependencyResult;
+import org.aikodi.chameleon.core.element.Element;
+import org.aikodi.chameleon.util.Util;
+import org.eclipse.gef4.zest.fx.ui.jface.IGraphNodeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+public class DependencyContentProvider implements IGraphNodeContentProvider {
+
+  @Override
+  public void dispose() {
+    _result = null;
+  }
+
+  @Override
+  public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+    _result = (DependencyResult) newInput;
+  }
+
+  private DependencyResult _result;
+
+  //	@Override
+  //	public Node getSource(Object rel) {
+  //		return ((UniEdge)rel).startNode();
+  //	}
+  //
+  //	@Override
+  //	public Node getDestination(Object rel) {
+  //		return ((UniEdge)rel).endNode();
+  //	}
+
+  @Override
+  public Object[] getConnectedTo(Object input) {
+    Object[] result;
+    if(_result != null) {
+      Set<Element> dependencies = _result.dependencies((Element) input);
+      result = dependencies.stream().collect(Collectors.toList()).toArray();
+    } else {
+      result = new Object[0];
+    }
+    return result;
+  }
+
+  /**
+   * @{inheritDoc}
+   */
+  @Override
+  public Object[] getNodes() {
+    Object[] result;
+    if(_result == null) {
+      result = new Object[0];
+    } else {
+      Set<Element> elements = _result.elements();
+      result = new Object[elements.size()];
+      int i=0;
+      for(Element e: elements) {
+        result[i] = e;
+        i++;
+      }
+    }
+    return result;
+  }
+
+}
--- src/org/aikodi/chameleon/eclipse/view/dependency/DependencyLabelProvider.java
+++ src/org/aikodi/chameleon/eclipse/view/dependency/DependencyLabelProvider.java
@@ -1,155 +1,147 @@
 package org.aikodi.chameleon.eclipse.view.dependency;
-//package be.kuleuven.cs.distrinet.chameleon.eclipse.view.dependency;
+
+import org.aikodi.chameleon.analysis.dependency.DependencyResult.DependencyCount;
+import org.aikodi.chameleon.core.element.Element;
+import org.aikodi.chameleon.core.namespace.Namespace;
+import org.aikodi.chameleon.core.variable.Variable;
+import org.aikodi.chameleon.eclipse.connector.EclipseEditorExtension;
+import org.aikodi.chameleon.exception.ModelException;
+import org.aikodi.chameleon.oo.type.Type;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+
+import be.kuleuven.cs.distrinet.rejuse.graph.Node;
+import be.kuleuven.cs.distrinet.rejuse.graph.UniEdge;
+
+class DependencyLabelProvider extends LabelProvider {//implements IConnectionStyleProvider, IEntityStyleProvider {
+
+
+  @Override
+  public String getText(Object element) {
+    if(element instanceof Node) {
+      element = ((Node)element).object();
+    }
+
+    if(element instanceof Element) {
+      return ((Element)element).language().plugin(EclipseEditorExtension.class).getLabel((Element) element);
+    } else if(element instanceof UniEdge) {
+      DependencyCount count = ((UniEdge<Element>)element).get(DependencyCount.class);
+      return ""+count.value();
+    } else {
+      return "";
+    }
+  }
+
+  @Override
+  public Image getImage(Object element) {
+    if(element instanceof Node) {
+      element = ((Node)element).object();
+    }
+    if(element instanceof Element) {
+      try {
+        return ((Element)element).language().plugin(EclipseEditorExtension.class).getIcon((Element) element);
+      } catch(ModelException exc) {
+        exc.printStackTrace();
+      }
+    }
+    return super.getImage(element);
+  }
+
+//  @Override
+//  public int getConnectionStyle(Object rel) {
+//    return ZestStyles.CONNECTIONS_SOLID;
+//  }
 //
-//import org.eclipse.draw2d.ConnectionRouter;
-//import org.eclipse.draw2d.IFigure;
-//import org.eclipse.draw2d.Label;
-//import org.eclipse.gef4.zest.core.viewers.IConnectionStyleProvider;
-//import org.eclipse.gef4.zest.core.viewers.IEntityStyleProvider;
-//import org.eclipse.gef4.zest.core.widgets.ZestStyles;
-//import org.eclipse.gef4.zest.core.widgets.decoration.DirectedConnectionDecorator;
-//import org.eclipse.gef4.zest.core.widgets.decoration.IConnectionDecorator;
-//import org.eclipse.jface.viewers.LabelProvider;
-//import org.eclipse.swt.SWT;
-//import org.eclipse.swt.graphics.Color;
-//import org.eclipse.swt.graphics.Image;
-//import org.eclipse.swt.widgets.Display;
+//  @Override
+//  public Color getColor(Object rel) {
+//    return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GRAY);
+//  }
 //
-//import be.kuleuven.cs.distrinet.chameleon.analysis.dependency.DependencyResult.DependencyCount;
-//import be.kuleuven.cs.distrinet.chameleon.core.element.Element;
-//import be.kuleuven.cs.distrinet.chameleon.core.namespace.Namespace;
-//import be.kuleuven.cs.distrinet.chameleon.eclipse.connector.EclipseEditorExtension;
-//import be.kuleuven.cs.distrinet.chameleon.exception.ModelException;
-//import be.kuleuven.cs.distrinet.chameleon.oo.type.Type;
-//import be.kuleuven.cs.distrinet.chameleon.oo.variable.Variable;
-//import be.kuleuven.cs.distrinet.rejuse.graph.Node;
-//import be.kuleuven.cs.distrinet.rejuse.graph.UniEdge;
+//  @Override
+//  public Color getHighlightColor(Object rel) {
+//    return Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
+//  }
 //
-//class DependencyLabelProvider extends LabelProvider implements IConnectionStyleProvider, IEntityStyleProvider {
-//		
-//		
-//		@Override
-//		public String getText(Object element) {
-//			if(element instanceof Node) {
-//				element = ((Node)element).object();
-//			}
-//			
-//			if(element instanceof Element) {
-//				return ((Element)element).language().plugin(EclipseEditorExtension.class).getLabel((Element) element);
-//			} else if(element instanceof UniEdge) {
-//				DependencyCount count = ((UniEdge<Element>)element).get(DependencyCount.class);
-//				return ""+count.value();
-//			} else {
-//				return "";
-//			}
-//		}
-//		
-//		@Override
-//		public Image getImage(Object element) {
-//			if(element instanceof Node) {
-//				element = ((Node)element).object();
-//			}
-//			if(element instanceof Element) {
-//				try {
-//				return ((Element)element).language().plugin(EclipseEditorExtension.class).getIcon((Element) element);
-//				} catch(ModelException exc) {
-//					exc.printStackTrace();
-//				}
-//			}
-//			return super.getImage(element);
-//		}
+//  @Override
+//  public int getLineWidth(Object rel) {
+//    return 1;
+//  }
 //
-//		@Override
-//		public int getConnectionStyle(Object rel) {
-//			return ZestStyles.CONNECTIONS_SOLID;
-//		}
+//  @Override
+//  public IFigure getTooltip(Object entity) {
+//    if(entity instanceof Node) {
+//      entity = ((Node)entity).object();
+//    }
+//    //FIXME: This is a temporary hack. This label provider should of course 
+//    // have no knowledge of types or variables.
+//    if(entity instanceof Type) {
+//      return new Label(((Type)entity).getFullyQualifiedName());
+//    } else if(entity instanceof Variable) {
+//      Type type = ((Variable) entity).nearestAncestor(Type.class);
+//      return new Label(type.getFullyQualifiedName()+"."+((Variable)entity).name());
+//    } else if(entity instanceof Namespace) {
+//      return new Label(((Namespace)entity).getFullyQualifiedName());
+//    } else if(entity instanceof UniEdge) {
+//      UniEdge<?> edge = (UniEdge) entity;
+//      return new Label(getText(edge.startNode().object()) + " has " + edge.get(DependencyCount.class).value()+" dependencies on "+getText(edge.endNode().object()));
+//    }
+//    return null;
+//  }
 //
-//		@Override
-//		public Color getColor(Object rel) {
-//			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GRAY);
-//		}
+//  @Override
+//  public Color getNodeHighlightColor(Object entity) {
+//    return Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_BACKGROUND_GRADIENT);
+//  }
 //
-//		@Override
-//		public Color getHighlightColor(Object rel) {
-//			return Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
-//		}
+//  @Override
+//  public Color getBorderColor(Object entity) {
+//    return Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
+//  }
 //
-//		@Override
-//		public int getLineWidth(Object rel) {
-//			return 1;
-//		}
+//  @Override
+//  public Color getBorderHighlightColor(Object entity) {
+//    return Display.getCurrent().getSystemColor(SWT.COLOR_BLACK);
+//  }
 //
-//		@Override
-//		public IFigure getTooltip(Object entity) {
-//			if(entity instanceof Node) {
-//				entity = ((Node)entity).object();
-//			}
-//			//FIXME: This is a temporary hack. This label provider should of course 
-//			// have no knowledge of types or variables.
-//			if(entity instanceof Type) {
-//			  return new Label(((Type)entity).getFullyQualifiedName());
-//			} else if(entity instanceof Variable) {
-//				Type type = ((Variable) entity).nearestAncestor(Type.class);
-//				return new Label(type.getFullyQualifiedName()+"."+((Variable)entity).name());
-//			} else if(entity instanceof Namespace) {
-//				return new Label(((Namespace)entity).getFullyQualifiedName());
-//			} else if(entity instanceof UniEdge) {
-//				UniEdge<?> edge = (UniEdge) entity;
-//				return new Label(getText(edge.startNode().object()) + " has " + edge.get(DependencyCount.class).value()+" dependencies on "+getText(edge.endNode().object()));
-//			}
-//			return null;
-//		}
+//  @Override
+//  public int getBorderWidth(Object entity) {
+//    return 1;
+//  }
 //
-//		@Override
-//		public Color getNodeHighlightColor(Object entity) {
-//			return Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_BACKGROUND_GRADIENT);
-//		}
+//  @Override
+//  public Color getBackgroundColour(Object entity) {
+//    if(entity instanceof Node) {
+//      Node node = (Node)entity;
+//      if(node.canReach(node)) {
+//        return new Color(Display.getCurrent(), 200, 50, 50);
+//      }
+//    } 
+//    return new Color(Display.getCurrent(), 100, 100, 100);
+//  }
 //
-//		@Override
-//		public Color getBorderColor(Object entity) {
-//			return Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
-//		}
+//  @Override
+//  public Color getForegroundColour(Object entity) {
+//    return Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_FOREGROUND);
+//  }
 //
-//		@Override
-//		public Color getBorderHighlightColor(Object entity) {
-//			return Display.getCurrent().getSystemColor(SWT.COLOR_BLACK);
-//		}
+//  @Override
+//  public boolean fisheyeNode(Object entity) {
+//    return false;
+//  }
 //
-//		@Override
-//		public int getBorderWidth(Object entity) {
-//			return 1;
-//		}
+//  @Override
+//  public ConnectionRouter getRouter(Object rel) {
+//    // Default implementation as per the javadocs of Zest 2.
+//    return null;
+//  }
 //
-//		@Override
-//		public Color getBackgroundColour(Object entity) {
-//			if(entity instanceof Node) {
-//				Node node = (Node)entity;
-//				if(node.canReach(node)) {
-//					return new Color(Display.getCurrent(), 200, 50, 50);
-//				}
-//			} 
-//			return new Color(Display.getCurrent(), 100, 100, 100);
-//		}
-//
-//		@Override
-//		public Color getForegroundColour(Object entity) {
-//			return Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_FOREGROUND);
-//		}
-//
-//		@Override
-//		public boolean fisheyeNode(Object entity) {
-//			return false;
-//		}
-//
-//		@Override
-//		public ConnectionRouter getRouter(Object rel) {
-//			// Default implementation as per the javadocs of Zest 2.
-//			return null;
-//		}
-//
-//		@Override
-//		public IConnectionDecorator getConnectionDecorator(Object rel) {
-//			return new DirectedConnectionDecorator();
-//		}
-//		
-//	}
+//  @Override
+//  public IConnectionDecorator getConnectionDecorator(Object rel) {
+//    return new DirectedConnectionDecorator();
+//  }
+
+}
