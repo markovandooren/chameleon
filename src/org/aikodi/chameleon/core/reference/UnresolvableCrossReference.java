49e457543fba9bf5590257152ce9ce2e9762fc96
Marko van Dooren <Marko.van.Dooren@gmail.com>
2016-05-31 01:28:36 +0200

    improved typing

--- src/org/aikodi/chameleon/core/reference/UnresolvableCrossReference.java
+++ src/org/aikodi/chameleon/core/reference/UnresolvableCrossReference.java
@@ -4,11 +4,11 @@ import org.aikodi.chameleon.core.validation.BasicProblem;
 
 public class UnresolvableCrossReference extends BasicProblem {
 
-	public UnresolvableCrossReference(CrossReference element) {
+	public UnresolvableCrossReference(CrossReference<?> element) {
 		super(element, "Cross reference could not be resolved.");
 	}
 
-	public UnresolvableCrossReference(CrossReference element, String msg) {
+	public UnresolvableCrossReference(CrossReference<?> element, String msg) {
 		super(element, "Cross reference could not be resolved: "+msg);
 	}
 
