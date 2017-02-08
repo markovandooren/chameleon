3c5e9f44ad5552e57bf7019d3e50545ca49bf7a1
Marko van Dooren <Marko.van.Dooren@gmail.com>
2017-02-08 20:07:59 +0100

    Added toString() + indent change

--- src/org/aikodi/chameleon/support/member/simplename/SimpleNameMethodInvocation.java
+++ src/org/aikodi/chameleon/support/member/simplename/SimpleNameMethodInvocation.java
@@ -1,6 +1,8 @@
 package org.aikodi.chameleon.support.member.simplename;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.aikodi.chameleon.core.event.name.NameChanged;
 import org.aikodi.chameleon.core.lookup.LookupException;
@@ -15,88 +17,97 @@ import org.aikodi.rejuse.logic.ternary.Ternary;
 
 public abstract class SimpleNameMethodInvocation<D extends Method> extends MethodInvocation<D> implements CrossReferenceWithName<D> {
 
-  public SimpleNameMethodInvocation(CrossReferenceTarget target, String name) {
-    super(target);
-    setName(name);
-  }
-  
-  @Override
-protected Type actualType() throws LookupException {
-//    try {
-			Method method = getElement();
-//			if (method != null) {
-			  return method.returnType();
-//			}
-//			else {
-//			  getElement();
-//			  throw new LookupException("Could not find method of constructor invocation", this);
-//			}
-//		} catch (LookupException e) {
-//			e.printStackTrace();
-//			getMethod();
-//			throw e;
-//		}
-  }
+	public SimpleNameMethodInvocation(CrossReferenceTarget target, String name) {
+		super(target);
+		setName(name);
+	}
+
+	@Override
+	protected Type actualType() throws LookupException {
+		//    try {
+		Method method = getElement();
+		//			if (method != null) {
+		return method.returnType();
+		//			}
+		//			else {
+		//			  getElement();
+		//			  throw new LookupException("Could not find method of constructor invocation", this);
+		//			}
+		//		} catch (LookupException e) {
+		//			e.printStackTrace();
+		//			getMethod();
+		//			throw e;
+		//		}
+	}
 
 
 
-  
-  /********
-   * NAME *
-   ********/
 
-  private String _methodName;
+	/********
+	 * NAME *
+	 ********/
 
-  @Override
-public String name() {
-    return _methodName;
-  }
+	private String _methodName;
 
-  @Override
-public void setName(String name) {
-    String old = _methodName;
-    _methodName = name;
-    if(changeNotificationEnabled()) {
-      notify(new NameChanged(old, name));
-    }
-  }
+	@Override
+	public String name() {
+		return _methodName;
+	}
 
-//  public D getMethod() throws LookupException {
-//	    D result = lexicalContext().lookUp(selector());
-//	    if(result == null) {
-//	      throw new LookupException();
-//	    }
-//	    return result;
-//  }
+	@Override
+	public void setName(String name) {
+		String old = _methodName;
+		_methodName = name;
+		if(changeNotificationEnabled()) {
+			notify(new NameChanged(old, name));
+		}
+	}
+
+	//  public D getMethod() throws LookupException {
+	//	    D result = lexicalContext().lookUp(selector());
+	//	    if(result == null) {
+	//	      throw new LookupException();
+	//	    }
+	//	    return result;
+	//  }
 
 
-  public abstract class SimpleNameMethodSelector extends SimpleNameCrossReferenceWithArgumentsSelector<D> {
-  	
-//  	private int _nameHash = SimpleNameMethodInvocation.this._methodName.hashCode();
-    
-  	@Override
-    public boolean selectedRegardlessOfName(D declaration) throws LookupException {
-  		boolean result = declaration.is(language(ObjectOrientedLanguage.class).CONSTRUCTOR) != Ternary.TRUE;
-  		if(result) {
-  			result = super.selectedRegardlessOfName(declaration);
-  		}
+	public abstract class SimpleNameMethodSelector extends SimpleNameCrossReferenceWithArgumentsSelector<D> {
 
-  		return result;
-    }
-  	
-  	@Override
-  	public int nbActualParameters() {
-  		return SimpleNameMethodInvocation.this.nbActualParameters();
-  	}
-  	
-  	@Override
-  	public List<Type> getActualParameterTypes() throws LookupException {
-  		return SimpleNameMethodInvocation.this.getActualParameterTypes();
-  	}
-  	
-  	@Override
-   public String name() {
-  		return SimpleNameMethodInvocation.this.name();
-  	}
-  }
+		//  	private int _nameHash = SimpleNameMethodInvocation.this._methodName.hashCode();
+
+		@Override
+		public boolean selectedRegardlessOfName(D declaration) throws LookupException {
+			boolean result = declaration.is(language(ObjectOrientedLanguage.class).CONSTRUCTOR) != Ternary.TRUE;
+			if(result) {
+				result = super.selectedRegardlessOfName(declaration);
+			}
+
+			return result;
+		}
+
+		@Override
+		public int nbActualParameters() {
+			return SimpleNameMethodInvocation.this.nbActualParameters();
+		}
+
+		@Override
+		public List<Type> getActualParameterTypes() throws LookupException {
+			return SimpleNameMethodInvocation.this.getActualParameterTypes();
+		}
+
+		@Override
+		public String name() {
+			return SimpleNameMethodInvocation.this.name();
+		}
+	}
+	
+	@Override
+	public String toString() {
+		Collector<CharSequence, ?, String> joiner = Collectors.joining(", ");
+		return (getTarget() == null ? "" : getTarget().toString()+".")
+				+name()
+				+"("+getActualParameters().stream().map(e -> e.toString()).collect(joiner)+")";
+	}
+	
 }
